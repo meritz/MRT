33b9d2bc497120f4e5990cf4696f266cabaa9b97
Unknown <modestemeritz@gmail.com>
2019-01-26 04:44:29 +0000

    config radion
    pour la parite lecteur radion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { RadioService } from './../services/radio.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  
+  isPlaying: fals;
+
+constructor( player: RadioService) {
+  this.isPlaying = player;
+}
+
+ngOnInit() {
 
 }
+
+startStopPlaying () {
+  // your logic to play pause
+  if (this.isPlaying) {
+    // pause here
+    this.pause();
+    console.log('pause');
+  } else {
+    // play here
+    this.isPlaying.play().then(() => {
+      console.log('Playing');
+    });
+
+    // this.play();
+    console.log('Playing');
+  }
+  this.isPlaying = !this.isPlaying;
+}
+
+play() {
+  this.isPlaying.play().then(() => {
+    console.log('Playing');
+  });
+}
+
+pause() {
+  this.isPlaying.pause();
+}
+
+}
+
