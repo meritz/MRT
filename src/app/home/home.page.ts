b953bcec3a1a87e7451e4eaee3fefd38d8935ba1
Unknown <modestemeritz@gmail.com>
2019-01-27 15:57:27 +0000

    fonction radio
    pour le button play/pause de la partie radio de la application

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RadioService } from './../services/radio.service';
+import { NavController } from '@ionic/angular';
+
+// import { RadioPage } from './../radio/radio.page';
+
 
 @Component({
   selector: 'app-home',
@@ -9,10 +13,11 @@ import { RadioService } from './../services/radio.service';
 })
 export class HomePage implements OnInit {
 
-  
-  isPlaying: fals;
 
-constructor( player: RadioService) {
+  isPlaying;
+  // anRadioPage: RadioPage;
+
+constructor( player: RadioService, public navCtrl: NavController) {
   this.isPlaying = player;
 }
 
@@ -20,33 +25,50 @@ ngOnInit() {
 
 }
 
-startStopPlaying () {
-  // your logic to play pause
-  if (this.isPlaying) {
-    // pause here
-    this.pause();
-    console.log('pause');
-  } else {
-    // play here
-    this.isPlaying.play().then(() => {
-      console.log('Playing');
-    });
 
-    // this.play();
-    console.log('Playing');
+// startStopPlaying () {
+//   // your logic to play pause
+//   if (this.isPlaying) {
+//     // pause here
+//     // this.pause();
+//     this.isPlaying.pause();
+//     // this.play();
+//   } else {
+//     // play here
+//     // this.play();
+//     // this.isPlaying.play();
+//     this.isPlaying.play().then(() => {
+//            console.log('Playing');
+//       });
+//     // this.pause();
+//     // console.log('Pause');
+//   }
+//   this.isPlaying = !this.isPlaying;
+// };
+
+startStopPlaying () {
+  if (this.isPlaying) {
+    this.isPlaying.play().then(() => {
+     console.log('playing');
+    });
+  } else {
+  this.isPlaying.pause = false;
+  console.log('pause');
   }
   this.isPlaying = !this.isPlaying;
 }
 
-play() {
-  this.isPlaying.play().then(() => {
-    console.log('Playing');
-  });
-}
 
-pause() {
-  this.isPlaying.pause();
-}
+
+//   play() {
+//   this.isPlaying.play().then(() => {
+//     console.log('Playing');
+//   });
+// }
+
+// pause = function() {
+//   this.isPlaying.pause();
+// };
 
 }
 
